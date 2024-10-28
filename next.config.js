2d07c8c46e1d87758ff2182fb747692ff91d556b
priYanshU739 <priyanshuranjancosmicx@gmail.com>
2024-10-28 21:30:17 +0530

    init

--- next.config.js
+++ next.config.js
@@ -30,7 +30,31 @@ const nextConfig = {
     }
     return config;
   },
-  output: 'standalone',
+  output: 'export',
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        // Example rewrite rule
+        source: '/old-page/:slug*',
+        destination: '/new-page/:slug*', // Update this to your actual paths
+      },
+      // You can add more rewrite rules here if needed
+    ];
+  },
 };
 
 module.exports = nextConfig;
