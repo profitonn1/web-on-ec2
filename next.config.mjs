0fe3da9f1879fcbc6b5313154aa1c884cc80ad45
priYanshU739 <priyanshuranjancosmicx@gmail.com>
2024-10-28 18:10:49 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
 
      output: 'export',
+     // next.config.js
+async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://js-next-nu.vercel.app/:path*",
+      },
+    ];
+   },
 };
 
 export default nextConfig;
